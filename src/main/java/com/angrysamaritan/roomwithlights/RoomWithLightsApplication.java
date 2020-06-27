a6de7f7bd7db5492789c0d665baca9b106589c0d
Andrei Trukhan <andrey-truhan@tut.by>
2020-06-27 15:12:07 +0300

    React base structure

---
+++ src/main/java/com/angrysamaritan/roomwithlights/RoomWithLightsApplication.java
@@ -0,0 +1,13 @@
+package com.angrysamaritan.roomwithlights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomWithLightsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomWithLightsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/angrysamaritan/roomwithlights/RoomWithLightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angrysamaritan.roomwithlights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomWithLightsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
