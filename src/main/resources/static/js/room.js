be46445b2aaaa11fa44314c26b26e370dd295bbf
Andrei Trukhan <andrey-truhan@tut.by>
2020-06-28 19:39:26 +0300

    Long poll requests

--- src/main/resources/static/js/room.js
+++ src/main/resources/static/js/room.js
@@ -1,26 +1,53 @@
+const HOST = "http://localhost:8081";
+const TIME_MILLISECONDS = 10000;
 let state;
+let roomId;
+let lock = false;
+
 $(document).ready(
     () => {
         state = $(".light_info").attr("value") === "on";
-        console.log(state);
+        roomId = $("#room_id").attr("value");
+        lock = true;
+        console.log(lock)
     }
 )
 
+setInterval(() => switchLightLongPoll());
 
+function switchLightLongPoll() {
+    console.log("All right");
+    if (lock) {
+        lock = false;
+        axios.get(HOST + "/room/long_poll?id=" + roomId + "&last_state=" + state + "&time=" + TIME_MILLISECONDS).then(
+            (response) => {
+                if (response.data !== state) {
+                    changeToAnother();
+                }
+            }
+        ).finally(() => {
+            lock = true
+        });
+    }
+}
 
-function switchLight(id) {
-    axios.post("http://localhost:8081/room/id" + id + "/switch").then(
+function switchLight() {
+    axios.post(HOST + "/room/id" + roomId + "/switch").then(
         (response) => {
             if (response.status === 200) {
-                if (state) {
-                    $(".room").attr("class", "room room_light_off");
-                    $(".switch").html("Switch light on");
-                } else {
-                    $(".room").attr("class", "room room_light_on");
-                    $(".switch").html("Switch light off");
-                }
-                state = !state;
+                changeToAnother();
             }
         }
     );
+}
+
+function changeToAnother() {
+    if (state) {
+        $(".room").attr("class", "room room_light_off");
+        $(".switch").html("Switch light on");
+    } else {
+        $(".room").attr("class", "room room_light_on");
+        $(".switch").html("Switch light off");
+    }
+    state = !state;
 }
